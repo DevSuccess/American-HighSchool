40cfbcb87540bfa95d1b7248584206e0e1ef925d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-17 10:38:01 +0300

    Mise au point des models

--- app/models.py
+++ app/models.py
@@ -2,332 +2,110 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
-from django.contrib import admin
-import os
-import imghdr
-
-# Create your models here.
-DAYS = (
-    ('Mon', 'Monday'),
-    ('Tue', 'Tuesday'),
-    ('Wed', 'Wednesday'),
-    ('Thu', 'Thursday'),
-    ('Fri', 'Friday'),
-    ('Sat', 'Saturday'),
-    ('Sun', 'Sunday'),
-)
-
-CONTACTS = (
-    ('fa-envelope', 'Email'),
-    ('fa-phone-alt', 'Phone'),
-    ('fa-whatsapp', 'WhatsApp'),
-)
-
-SOCIALS = (
-    ('fa-linkedin-in', 'LinkedIn'),
-    ('fa-twitter', 'Twitter'),
-    ('fa-facebook-f', 'Facebook'),
-    ('fa-instagram', 'Instagram'),
-)
-
-STATES = sorted([
-    ('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands'),
-    ('Andhra Pradesh', 'Andhra Pradesh'),
-    ('Arunachal Pradesh', 'Arunachal Pradesh'),
-    ('Assam', 'Assam'),
-    ('Bihar', 'Bihar'),
-    ('Chandigarh', 'Chandigarh'),
-    ('Chhattisgarh', 'Chhattisgarh'),
-    ('Dadra & Nagar Haveli', 'Dadra & Nagar Haveli'),
-    ('Daman & Diu', 'Daman & Diu'),
-    ('Delhi', 'Delhi'),
-    ('Goa', 'Goa'),
-    ('Gujarat', 'Gujarat'),
-    ('Haryana', 'Haryana'),
-    ('Himachal Pradesh', 'Himachal Pradesh'),
-    ('Jammu & Kashmir', 'Jammu & Kashmir'),
-    ('Jharkhand', 'Jharkhand'),
-    ('Karnataka', 'Karnataka'),
-    ('Kerala', 'Kerala'),
-    ('Lakshadweep', 'Lakshadweep'),
-    ('Madhya Pradesh', 'Madhya Pradesh'),
-    ('Maharashtra', 'Maharashtra'),
-    ('Manipur', 'Manipur'),
-    ('Meghalaya', 'Meghalaya'),
-    ('Mizoram', 'Mizoram'),
-    ('Nagaland', 'Nagaland'),
-    ('Odisha', 'Odisha'),
-    ('Puducherry', 'Puducherry'),
-    ('Punjab', 'Punjab'),
-    ('Rajasthan', 'Rajasthan'),
-    ('Sikkim', 'Sikkim'),
-    ('Tamil Nadu', 'Tamil Nadu'),
-    ('Telangana', 'Telangana'),
-    ('Tripura', 'Tripura'),
-    ('Uttar Pradesh', 'Uttar Pradesh'),
-    ('Uttarakhand', 'Uttarakhand'),
-    ('Afghanistan', 'Afghanistan'),
-    ('Albania', 'Albania'),
-    ('Algeria', 'Algeria'),
-    ('Andorra', 'Andorra'),
-    ('Angola', 'Angola'),
-    ('Antigua and Barbuda', 'Antigua and Barbuda'),
-    ('Argentina', 'Argentina'),
-    ('Armenia', 'Armenia'),
-    ('Australia', 'Australia'),
-    ('Austria', 'Austria'),
-    ('Azerbaijan', 'Azerbaijan'),
-    ('Bahamas', 'Bahamas'),
-    ('Bahrain', 'Bahrain'),
-    ('Bangladesh', 'Bangladesh'),
-    ('Barbados', 'Barbados'),
-    ('Belarus', 'Belarus'),
-    ('Belgium', 'Belgium'),
-    ('Belize', 'Belize'),
-    ('Benin', 'Benin'),
-    ('Bhutan', 'Bhutan'),
-    ('Bolivia', 'Bolivia'),
-    ('Bosnia and Herzegovina', 'Bosnia and Herzegovina'),
-    ('Botswana', 'Botswana'),
-    ('Brazil', 'Brazil'),
-    ('Brunei', 'Brunei'),
-    ('Bulgaria', 'Bulgaria'),
-    ('Burkina Faso', 'Burkina Faso'),
-    ('Burundi', 'Burundi'),
-    ('Cabo Verde', 'Cabo Verde'),
-    ('Cambodia', 'Cambodia'),
-    ('Cameroon', 'Cameroon'),
-    ('Canada', 'Canada'),
-    ('Central African Republic', 'Central African Republic'),
-    ('Chad', 'Chad'),
-    ('Chile', 'Chile'),
-    ('China', 'China'),
-    ('Colombia', 'Colombia'),
-    ('Comoros', 'Comoros'),
-    ('Congo, Democratic Republic of the', 'Congo, Democratic Republic of the'),
-    ('Congo, Republic of the', 'Congo, Republic of the'),
-    ('Costa Rica', 'Costa Rica'),
-    ('Croatia', 'Croatia'),
-    ('Cuba', 'Cuba'),
-    ('Cyprus', 'Cyprus'),
-    ('Czech Republic', 'Czech Republic'),
-    ('Denmark', 'Denmark'),
-    ('Djibouti', 'Djibouti'),
-    ('Dominica', 'Dominica'),
-    ('Dominican Republic', 'Dominican Republic'),
-    ('East Timor', 'East Timor'),
-    ('Ecuador', 'Ecuador'),
-    ('Egypt', 'Egypt'),
-    ('El Salvador', 'El Salvador'),
-    ('Equatorial Guinea', 'Equatorial Guinea'),
-    ('Eritrea', 'Eritrea'),
-    ('Estonia', 'Estonia'),
-    ('Eswatini', 'Eswatini'),
-    ('Ethiopia', 'Ethiopia'),
-    ('Fiji', 'Fiji'),
-    ('Finland', 'Finland'),
-    ('France', 'France'),
-    ('Gabon', 'Gabon'),
-    ('Gambia', 'Gambia'),
-    ('Georgia', 'Georgia'),
-    ('Germany', 'Germany'),
-    ('Ghana', 'Ghana'),
-    ('Greece', 'Greece'),
-    ('Grenada', 'Grenada'),
-    ('Guatemala', 'Guatemala'),
-    ('Guinea', 'Guinea'),
-    ('Guinea-Bissau', 'Guinea-Bissau'),
-    ('Guyana', 'Guyana'),
-    ('Haiti', 'Haiti'),
-    ('Honduras', 'Honduras'),
-    ('Hungary', 'Hungary'),
-    ('Iceland', 'Iceland'),
-    ('India', 'India'),
-    ('Indonesia', 'Indonesia'),
-    ('Iran', 'Iran'),
-    ('Iraq', 'Iraq'),
-    ('Ireland', 'Ireland'),
-    ('Israel', 'Israel'),
-    ('Italy', 'Italy'),
-    ('Jamaica', 'Jamaica'),
-    ('Japan', 'Japan'),
-    ('Jordan', 'Jordan'),
-    ('Kazakhstan', 'Kazakhstan'),
-    ('Kenya', 'Kenya'),
-    ('Kiribati', 'Kiribati'),
-    ('Korea, North', 'Korea, North'),
-    ('Korea, South', 'Korea, South'),
-    ('Kosovo', 'Kosovo'),
-    ('Kuwait', 'Kuwait'),
-    ('Kyrgyzstan', 'Kyrgyzstan'),
-    ('Laos', 'Laos'),
-    ('Latvia', 'Latvia'),
-    ('Lebanon', 'Lebanon'),
-    ('Lesotho', 'Lesotho'),
-    ('Liberia', 'Liberia'),
-    ('Libya', 'Libya'),
-    ('Liechtenstein', 'Liechtenstein'),
-    ('Lithuania', 'Lithuania'),
-    ('Luxembourg', 'Luxembourg'),
-    ('Madagascar', 'Madagascar'),
-    ('Malawi', 'Malawi'),
-    ('Malaysia', 'Malaysia'),
-    ('Maldives', 'Maldives'),
-    ('Mali', 'Mali'),
-    ('Malta', 'Malta'),
-    ('Marshall Islands', 'Marshall Islands'),
-    ('Mauritania', 'Mauritania'),
-    ('Mauritius', 'Mauritius'),
-    ('Mexico', 'Mexico'),
-    ('Micronesia', 'Micronesia'),
-    ('Moldova', 'Moldova'),
-    ('Monaco', 'Monaco'),
-    ('Mongolia', 'Mongolia'),
-    ('Montenegro', 'Montenegro'),
-    ('Morocco', 'Morocco'),
-    ('Mozambique', 'Mozambique'),
-    ('Myanmar', 'Myanmar'),
-    ('Namibia', 'Namibia'),
-    ('Nauru', 'Nauru'),
-    ('Nepal', 'Nepal'),
-    ('Netherlands', 'Netherlands'),
-    ('New Zealand', 'New Zealand'),
-    ('Nicaragua', 'Nicaragua'),
-    ('Niger', 'Niger'),
-    ('Nigeria', 'Nigeria'),
-    ('North Macedonia', 'North Macedonia'),
-    ('Norway', 'Norway'),
-    ('Oman', 'Oman'),
-    ('Pakistan', 'Pakistan'),
-    ('Palau', 'Palau'),
-    ('Palestine', 'Palestine'),
-    ('Panama', 'Panama'),
-    ('Papua New Guinea', 'Papua New Guinea'),
-    ('Paraguay', 'Paraguay'),
-    ('Peru', 'Peru'),
-    ('Philippines', 'Philippines'),
-    ('Poland', 'Poland'),
-    ('Portugal', 'Portugal'),
-    ('Qatar', 'Qatar'),
-    ('Romania', 'Romania'),
-    ('Russia', 'Russia'),
-    ('Rwanda', 'Rwanda'),
-    ('Saint Kitts and Nevis', 'Saint Kitts and Nevis'),
-    ('Saint Lucia', 'Saint Lucia'),
-    ('Saint Vincent and the Grenadines', 'Saint Vincent and the Grenadines'),
-    ('Samoa', 'Samoa'),
-    ('San Marino', 'San Marino'),
-    ('Sao Tome and Principe', 'Sao Tome and Principe'),
-    ('Saudi Arabia', 'Saudi Arabia'),
-    ('Senegal', 'Senegal'),
-    ('Serbia', 'Serbia'),
-    ('Seychelles', 'Seychelles'),
-    ('Sierra Leone', 'Sierra Leone'),
-    ('Singapore', 'Singapore'),
-    ('Slovakia', 'Slovakia'),
-    ('Slovenia', 'Slovenia'),
-    ('Solomon Islands', 'Solomon Islands'),
-    ('Somalia', 'Somalia'),
-    ('South Africa', 'South Africa'),
-    ('South Sudan', 'South Sudan'),
-    ('Spain', 'Spain'),
-    ('Sri Lanka', 'Sri Lanka'),
-    ('Sudan', 'Sudan'),
-    ('Suriname', 'Suriname'),
-    ('Sweden', 'Sweden'),
-    ('Switzerland', 'Switzerland'),
-    ('Syria', 'Syria'),
-    ('Taiwan', 'Taiwan'),
-    ('Tajikistan', 'Tajikistan'),
-    ('Tanzania', 'Tanzania'),
-    ('Thailand', 'Thailand'),
-    ('Togo', 'Togo'),
-    ('Tonga', 'Tonga'),
-    ('Trinidad and Tobago', 'Trinidad and Tobago'),
-    ('Tunisia', 'Tunisia'),
-    ('Turkey', 'Turkey'),
-    ('Turkmenistan', 'Turkmenistan'),
-    ('Tuvalu', 'Tuvalu'),
-    ('Uganda', 'Uganda'),
-    ('Ukraine', 'Ukraine'),
-    ('United Arab Emirates', 'United Arab Emirates'),
-    ('United Kingdom', 'United Kingdom'),
-    ('United States', 'United States'),
-    ('Uruguay', 'Uruguay'),
-    ('Uzbekistan', 'Uzbekistan'),
-    ('Vanuatu', 'Vanuatu'),
-    ('Vatican City', 'Vatican City'),
-    ('Venezuela', 'Venezuela'),
-    ('Vietnam', 'Vietnam'),
-    ('Yemen', 'Yemen'),
-    ('Zambia', 'Zambia'),
-    ('Zimbabwe', 'Zimbabwe')
-], key=lambda x: x[1])
+from django.core.validators import MaxValueValidator, MinValueValidator
+from .constants import ACADEMICS, DAYS, CONTACTS, STATES, SOCIALS, upload_path
 
 
-def filetype(filename):
-    # Vérification si c'est une image
-    image_type = imghdr.what(filename)
-    if image_type:
-        return 'image'
-
-    # Vérification si c'est une vidéo (vous pouvez ajouter d'autres extensions de fichier vidéo si nécessaire)
-    video_extensions = ['.mp4', '.avi', '.mov']
-    if any(filename.lower().endswith(ext) for ext in video_extensions):
-        return 'video'
-
-    # Aucun type correspondant trouvé
-    return 'unknown'
-
-
-def upload_path(instance, filename):
-    title = instance.title.replace("'", "")
-    # Remplacer les espaces par des underscores et mettre tout en minuscule
-    title = title.lower().replace(" ", "_")
-    title = title.lower().replace(".", "_")
-    # Retourner le chemin complet avec le nom du fichier
-    return os.path.join(str(filetype(filename)), f"{title}", filename)
-
-
-# Create your models here.
-class Contact(models.Model):
-    contact = models.CharField(max_length=200)
-    contact_type = models.CharField(max_length=25, choices=CONTACTS)
+class BaseModel(models.Model):
     active = models.BooleanField(default=True, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.contact
-
-
-class Social(models.Model):
-    network_name = models.CharField(max_length=200)
-    social_type = models.CharField(max_length=25, choices=SOCIALS)
-    url = models.URLField()
-    active = models.BooleanField(default=True, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.network_name
+    class Meta:
+        abstract = True
 
 
-class Address(models.Model):
+class ImageModel(models.Model):
+    image = models.FileField(
+        upload_to=upload_path,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['png', 'jpg', 'gif', 'jpeg', 'webp', 'svg', 'bmp'])
+        ],
+        default='',
+        null=True,
+        blank=True
+    )
+
+    def admin_photo(self):
+        return mark_safe(f"<a href='{self.image.url}'><img src='{self.image.url}' width='100' /></a>")
+
+    class Meta:
+        abstract = True
+
+
+class VideoModel(models.Model):
+    file = models.FileField(
+        upload_to=upload_path,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['mp4', 'avi', 'mov'])
+        ], default='', null=True, blank=True)
+
+    def admin_video(self):
+        return mark_safe(f"<video width='150' controls ><source  type='video/mp4' src='{self.file.url}'></video>")
+
+    class Meta:
+        abstract = True
+
+
+class Address(BaseModel):
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100, choices=STATES)
     url = models.TextField(null=True, blank=True)
     zip_code = models.CharField(max_length=10)
-    active = models.BooleanField(default=True, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.street
 
     def admin_url(self):
         return mark_safe(f"{self.url}")
+
+
+class AboutList(models.Model):
+    title = models.CharField(max_length=150)
+    status = models.BooleanField(default=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class About(BaseModel, ImageModel):
+    title = models.CharField(max_length=150)
+    libel = models.CharField(max_length=250)
+    lists = models.ForeignKey(AboutList, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Activity(BaseModel, ImageModel):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Collaborator(BaseModel, ImageModel):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Accreditation(BaseModel, ImageModel):
+    content = models.CharField(max_length=150)
+    collaborators = models.ForeignKey(Collaborator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
+class Contact(BaseModel):
+    contact = models.CharField(max_length=200)
+    contact_type = models.CharField(max_length=25, choices=CONTACTS)
+
+    def __str__(self):
+        return self.contact
 
 
 class Hour(models.Model):
@@ -345,51 +123,108 @@ class Hour(models.Model):
         return self.day
 
     def save(self, *args, **kwargs):
-        self.full_clean()  # Valider avant de sauvegarder
+        self.full_clean()
         super().save(*args, **kwargs)
 
 
-class Information(models.Model):
-    name = models.CharField(max_length=150)
-    addresses = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
-    contacts = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
-    hours = models.ForeignKey(Hour, on_delete=models.CASCADE)
-    socials = models.ForeignKey(Social, on_delete=models.CASCADE)
+# class Information(models.Model):
+#     name = models.CharField(max_length=150)
+#     addresses = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
+#     contacts = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
+#     hours = models.ForeignKey(Hour, on_delete=models.CASCADE)
+#     socials = models.ForeignKey(Social, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def clean(self):
+#         exist_records = Information.objects.count()
+#         if exist_records >= 1 and not self.pk:
+#             raise ValidationError('One value only')
+#
+#     def save(self, *args, **kwargs):
+#         self.full_clean()
+#         super().save(*args, **kwargs)
+
+
+class Members(BaseModel, ImageModel):
+    lastname = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=150)
+    occupation = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250, unique=True)
+    contacts = models.ForeignKey(Contact, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.lastname
+
+
+class Possibility(BaseModel):
+    value = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.value
+
+
+class Price(BaseModel, ImageModel):
+    academic = models.CharField(max_length=10, choices=ACADEMICS, unique=True)
+    price = models.FloatField()
+    registration = models.FloatField()
+    promotion = models.IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+    birth = models.IntegerField(
+        validators=[
+            MaxValueValidator(70),
+            MinValueValidator(2)
+        ],
+        choices=[(i, str(i)) for i in range(72)]
+    )
+    possibilities = models.ForeignKey(Possibility, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.academic
+
+
+class Service(BaseModel, ImageModel):
+    label = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.label
+
+
+class Social(BaseModel):
+    network_name = models.CharField(max_length=200)
+    social_type = models.CharField(max_length=25, choices=SOCIALS)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.network_name
+
+
+class Testimonial(BaseModel, ImageModel):
+    name = models.CharField(max_length=250)
+    content = models.TextField()
+    start = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ],
+        choices=[(i, str(i)) for i in range(6)]
+    )
+    occupation = models.CharField(max_length=150)
 
     def __str__(self):
         return self.name
 
-    def clean(self):
-        exist_records = Information.objects.count()
-        if exist_records >= 1 and not self.pk:
-            raise ValidationError('One values only')
 
-    def save(self, *args, **kwargs):
-        self.full_clean()  # Valider avant de sauvegarder
-        super().save(*args, **kwargs)
-
-
-class AboutList(models.Model):
-    title = models.CharField(max_length=150)
-    status = models.BooleanField(default=True, null=True)
-
-    def __str__(self):
-        return self.title
-
-
-class About(models.Model):
-    title = models.CharField(max_length=150)
-    libel = models.CharField(max_length=250)
-    active = models.BooleanField(default=True)
-    image = models.FileField(
-        upload_to=upload_path,
-        validators=[
-            FileExtensionValidator(allowed_extensions=['png', 'jpg', 'gif', 'jpeg', 'webp', 'svg', 'bmp'])
-        ], default='', null=True, blank=True)
+class Value(BaseModel, ImageModel):
     content = models.TextField()
-    lists = models.ForeignKey(AboutList, on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now=True, null=True)
-    updated_at = models.DateTimeField(auto_now_add=True, null=True)
 
-    def admin_photo(self):
-        return mark_safe(f"<a href='{self.image.url}'><img src='{self.image.url}' width='100' /><a/>")
+
+class Vision(BaseModel, ImageModel):
+    title = models.CharField(max_length=150, blank=True)
+    content = models.TextField()
