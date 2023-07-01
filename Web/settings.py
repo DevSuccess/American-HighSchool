9e9495380b79b29a61968214fc0f5657b4153f44
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-01 06:15:06 +0300

    Ajout de config local env et settings

--- Web/settings.py
+++ Web/settings.py
@@ -107,19 +107,19 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 # MySQL
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
         'ENGINE': 'mysql.connector.django',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
             'autocommit': True,
             'sql_mode': 'STRICT_TRANS_TABLES',
         },
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
