b4e5f469bcfa96c810c52ba8c9729ffd1781ef2c
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-03 10:19:13 +0300

    Changer le env

--- Web/settings.py
+++ Web/settings.py
@@ -7,18 +7,18 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 if DEBUG:
     ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
 else:
-    ALLOWED_HOSTS = ['*', os.environ.get('ALLOWED_HOSTS')]
+    ALLOWED_HOSTS = ['*', os.getenv('ALLOWED_HOSTS')]
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 if not SECRET_KEY:
     raise ValueError("SECRET key is not set in environment variables")
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'autocommit': True,
             'sql_mode': 'STRICT_TRANS_TABLES',
@@ -128,10 +128,10 @@ TIME_ZONE = 'Indian/Antananarivo'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, os.environ.get('STATIC_ROOT'))
+STATIC_ROOT = os.path.join(BASE_DIR, os.getenv('STATIC_ROOT', 'staticfiles'))
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, os.environ.get('MEDIA_ROOT'))
+MEDIA_ROOT = os.path.join(BASE_DIR, os.getenv('MEDIA_ROOT', 'media'))
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
