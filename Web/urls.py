941898b1d15d5dfc3917958a145a8dedafb40389
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-12 20:57:47 +0300

    Error 404 test 5

--- Web/urls.py
+++ Web/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.views.static import serve
 from django.urls import path, include, re_path
 
-#
-# handler404 = 'home.views.custom_404'
+
+handler404 = 'home.views.custom_404'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
