e5136cbf925c8fa35d704c45bd3c9ed597e08b90
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-13 11:24:09 +0300

    Rectification de admin 3

--- new/admin.py
+++ new/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from .models import UserPost, Post
 
+
 # Register your models here.
 @admin.register(UserPost)
 class UserPostAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'status', 'created_on')
+    list_display = ('title', 'status', 'created_on')
     list_filter = ('status', 'created_on')
-    search_fields = ('title', 'author__name')
+    search_fields = ('title',)
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'created_on'
     ordering = ('-created_on',)
