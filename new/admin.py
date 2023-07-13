126a99abdf31d133669a99fa22f77e662e7e4aec
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-13 10:50:18 +0300

    Ajout de amin.py

--- new/admin.py
+++ new/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import UserPost, Post
+
 
 # Register your models here.
+
+@admin.register(UserPost)
+class UserPostAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+    search_fields = ('author__name', 'content')
+    date_hierarchy = 'created_on'
+    readonly_fields = ('created_on', 'updated_on')
+
+    fieldsets = (
+        ('Post Details', {
+            'fields': ('author', 'content', 'image')
+        }),
+        ('Status', {
+            'fields': ('status',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_on', 'updated_on'),
+            'classes': ('collapse',)
+        })
+    )
