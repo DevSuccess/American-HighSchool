c6caa3e8a1b877b3387ea5ba404a24e05f72d866
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-14 20:44:51 +0300

    Ajout d'information abouts

--- about/views.py
+++ about/views.py
@@ -4,13 +4,9 @@ from . import models
 
 # Create your views here.
 def index(request):
-    mission = models.Info.objects.filter(type='A')
-    vision = models.Info.objects.filter(type='B')
-    organisation = models.Info.objects.filter(type='C')
+    aboutes = models.Info.objects.all()
     context = {
         'current_page': request.path,
-        'mission': mission,
-        'vision': vision,
-        'organisation': organisation
+        'aboutes': aboutes
     }
     return render(request, 'about/index.html', context)
