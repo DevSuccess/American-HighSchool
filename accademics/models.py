5702a8b5c8260633fbff30469d254d65036b98fa
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-15 13:40:44 +0300

    Rectification de bug

--- accademics/models.py
+++ accademics/models.py
@@ -5,11 +5,11 @@ from Web.utils import BaseModel, ImageModel
 
 # Create your models here.
 class Academic(BaseModel):
-    value = models.ForeignKey(mdl.Level, on_delete=models.CASCADE, null=True)
-    description = models.TextField()
+    value = models.ForeignKey(mdl.Level, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Les academies au sein de l'AHS"
 
     def __str__(self):
-        return self.value
+        return self.description
