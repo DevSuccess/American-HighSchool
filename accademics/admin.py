0294305f190499747c8205316e7f47301e4c524e
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-15 13:42:14 +0300

    Teste

--- accademics/admin.py
+++ accademics/admin.py
@@ -4,7 +4,7 @@ from .models import Academic
 
 @admin.register(Academic)
 class AcademicAdmin(admin.ModelAdmin):
-    list_display = ('value', 'created_at', 'updated_at')
+    list_display = ('description', 'created_at', 'updated_at')
     search_fields = ('value', 'description')
     fields = ('value', 'description', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
