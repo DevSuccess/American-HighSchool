baa928a2114c83e00021c20e4c8982b5ae328f9c
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-15 13:46:07 +0300

    Reactification de admin

--- accademics/admin.py
+++ accademics/admin.py
@@ -4,7 +4,13 @@ from .models import Academic
 
 @admin.register(Academic)
 class AcademicAdmin(admin.ModelAdmin):
-    list_display = ('description', 'created_at', 'updated_at')
+    list_display = ('value', 'created_at', 'updated_at')
     search_fields = ('value', 'description')
-    fields = ('value', 'description', 'created_at', 'updated_at')
+    list_filter = ('value',)
+
+    fieldsets = (
+        ('Academic Details', {
+            'fields': ('value', 'description', 'created_at', 'updated_at'),
+        }),
+    )
     readonly_fields = ('created_at', 'updated_at')
