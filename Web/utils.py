6abc535481f1f6d1f5ae5c8670d978a992f40b93
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-17 12:57:04 +0300

    Rectification

--- Web/utils.py
+++ Web/utils.py
@@ -5,13 +5,13 @@ from django.core.validators import FileExtensionValidator
 from django.utils.safestring import mark_safe
 
 DAYS = (
-    (1, 'Monday'),
-    (2, 'Tuesday'),
-    (3, 'Wednesday'),
-    (4, 'Thursday'),
-    (5, 'Friday'),
-    (6, 'Saturday'),
-    (7, 'Sunday')
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday')
 )
 
 SOCIALS = (
