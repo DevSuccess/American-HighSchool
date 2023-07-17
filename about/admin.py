ebce183f0a65019ccd7ae2147091438e57db9449
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-17 13:10:01 +0300

    Rectification de admin about

--- about/admin.py
+++ about/admin.py
@@ -4,9 +4,15 @@ from .models import Support, Info
 
 @admin.register(Support)
 class SupportAdmin(admin.ModelAdmin):
-    list_display = ('title', 'email')
+    list_display = ('title', 'email', 'display_contacts')
     filter_horizontal = ('contacts',)
+    search_fields = ('title', 'email')
     fields = ('title', 'email', 'contacts')
+
+    def display_contacts(self, obj):
+        return ', '.join(str(contact) for contact in obj.contacts.all())
+
+    display_contacts.short_description = 'Contacts'
 
 
 @admin.register(Info)
